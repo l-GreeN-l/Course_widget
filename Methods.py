7521dc4326b5fd71e466705fab5a694d2c33821c
Z_green_Z <valdisvan@mail.ru>
2022-02-07 19:18:08 +0300

    Update Methods.py

--- Methods.py
+++ Methods.py
@@ -1,16 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 def get_course():
-
     headers = {
         'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'}
-
     url = 'http://www.cbr.ru/'
-
+    
     response = requests.get(url, headers=headers)
-
+    
     if response.status_code == 200:
 
         soup = BeautifulSoup(response.content, 'html.parser')
@@ -22,6 +19,4 @@ def get_course():
             # print(elem)
         dol = elements[0].text.split('\r')[0]
         eur = elements[2].text.split('\r')[0]
-
-
     return [dol,eur]
